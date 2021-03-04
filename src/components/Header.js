5f6dba6f48fb466a74bb0ec1fbc9420f1130b430
Bryan Borgesano <BryanBorgesano97@gmail.com>
2021-03-04 15:30:25 -0500

    Added header

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  appBar: {
+    position: "relative",
+  },
+}));
+export default function Header() {
+  const classes = useStyles();
+
+  return (
+    <AppBar position="absolute" color="default" className={classes.appBar}>
+      <Toolbar>
+        <Typography variant="h6" color="inherit" noWrap>
+          Open Sponsorship Dev Test - Bryan Borgesano
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+}
